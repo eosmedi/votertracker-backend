9214fd879b7034ffabd158ecdf90c341ae3a6f02
fun <lljxx1@gmail.com>
2018-10-12 16:15:34 +0800

    only show registed proxy

--- app.js
+++ app.js
@@ -787,7 +787,7 @@ app.get('/search', searchApi);
 app.get('/getVoteProxy', function(req, res, next){
   var page = req.query.p || 1;
   var size = req.query.size || 50;
-  var type = req.query.reg || 'reg';
+  var type = req.query.type || 'reg';
 
   var data = getVoters(proxyVoters, true);
   data = data.filter((item) => {
